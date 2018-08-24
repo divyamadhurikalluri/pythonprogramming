336821dfb184967871e3c76bcfd204fc0703ff94
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2018-08-24 14:17:50 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,4 +1,4 @@
-ch='str(raw-input)'
+ch='A'
 if ch.isalpha():
     if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I'or ch=='O' or ch=='U'):
         print('vowel')
