a77c5e5c664f3d03dabfb743902514bee316f7e2
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-02-17 10:10:53 +0530

    Create 7-hunt.py

---
+++ 7-hunt.py
@@ -0,0 +1,14 @@
+N,B=input(),raw_input().split()
+A=[]
+if N==len(B):
+  for i in range(0,len(B)):
+    if i%2==0:
+      B[i]=int(B[i])
+      if ((B[i]%2)!=0):
+        A.append(B[i])
+    else:
+      B[i]=int(B[i])
+      if B[i]%2==0:
+        A.append(B[i])
+print (' '.join(map(str,A)))        
+ 
