2ac92a5716507a8582029cb2d40f37556e30a01d
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-02-17 20:43:34 +0530

    Update and rename holiday,11.py to holiday'11.py

---
+++ holiday'11.py
@@ -0,0 +1,6 @@
+x=['monday','tuesday','wednesday','thursday','friday','Monday','Tuesday','Wednesday','Thursday','Friday','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY']
+y=str(raw_input())
+if(y in x):
+  print('no')
+else:
+  print('yes')  
--- holiday,11.py
+++
@@ -1,6 +0,0 @@
-x=['monday','tuesday','wednesday','thursday','friday']
-y=str(raw_input())
-if(y in x):
-  print('no')
-else:
-  print('yes')  
