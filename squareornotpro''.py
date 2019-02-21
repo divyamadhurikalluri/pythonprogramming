7b0edc72d6f2798bf90c1d77847cfcfc60178fcb
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-02-21 20:09:19 +0530

    Create squareornotpro''.py

---
+++ squareornotpro''.py
@@ -0,0 +1,9 @@
+A,B=map(int,raw_input().split())        
+M,N=map(int,raw_input().split())        
+P,Q=map(int,raw_input().split())        
+X,Y=map(int,raw_input().split())
+if((A+M+P+X)==(B+N+Q+Y)):
+  print("yes")
+else:
+  print("no")  
+
