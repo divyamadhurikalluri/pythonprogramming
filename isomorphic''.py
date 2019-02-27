63df96b89e43539c2b402f4713ce3f0eef429251
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-02-27 19:26:19 +0530

    Update isomorphic''.py

--- isomorphic''.py
+++ isomorphic''.py
@@ -7,3 +7,13 @@ if(n>0):
   print("no")
 else:
   print("yes")
+'''
+s1,s2=raw_input().split()
+n=0
+for x in range(0,len(s1)):
+  if((ord(s1[x])-ord(s1[x-1]))==(ord(s2[x])-ord(s2[x-1]))):
+    print("yes")
+    break
+else:
+  print("no")
+ '''
