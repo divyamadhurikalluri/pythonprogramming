4f1239f75fd82437ced573466fa6663b459fc8b4
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-03-05 19:14:22 +0530

    Update aiplusajequalak''.py

--- aiplusajequalak''.py
+++ aiplusajequalak''.py
@@ -1,7 +1,7 @@
 N=int(raw_input())
 l=list(map(int,raw_input().split()))
-for i in range(0,len(l)-2):
-  for j in range(i+1,len(l)-1):
+for i in range(0,len(l)):
+  for j in range(i+1,len(l)):
     for k in range(j+1,len(l)):
       if l[i]+l[j]==l[k] and i<j<k:
         a=l[i],l[j],l[k]
