8e92d91ddf3b41cf0ab314247f8465ecb7e19df2
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-03-24 08:47:52 +0530

    Create studentschampoinship''.py

---
+++ studentschampoinship''.py
@@ -0,0 +1,14 @@
+N,k=map(eval,(raw_input()).split())
+a=list(map(int,(raw_input()).split()))
+s=0
+for i in a:
+	if i<=(5-k):
+		s+=1
+'''
+c=0
+while c<len(a):
+  if a[c]+k<=5:
+    s+=1
+  c+=1
+'''  
+print(s//3)
