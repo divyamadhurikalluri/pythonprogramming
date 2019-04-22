b1536e978a4df37a40f41d00243d3eb938029ae7
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-04-22 14:18:02 +0530

    Update and rename cyclicrotatebyktimes''.py to cycliccrotatebyktimes''.py

--- cycliccrotatebyktimes''.py
+++ cycliccrotatebyktimes''.py
@@ -1,10 +1,12 @@
 N,K= map(int,raw_input().split())
 L= list(map(int,raw_input().split()))
+K=K%N
 a= L[-K:] + L[:-K]
 print(' '.join(map(str,a)))
 '''
 N,K= map(int,input().split())
 L= list(map(int,input().split()))
+K=K%N
 a= L[-K:] + L[:-K]
 print(*a)
 '''
