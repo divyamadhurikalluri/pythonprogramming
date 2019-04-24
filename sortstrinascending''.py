fe7465e65d47979408160710d539aee97dc2bd10
divyamadhurikalluri <42571351+divyamadhurikalluri@users.noreply.github.com>
2019-04-24 19:42:21 +0530

    Create sortstrinascending''.py

---
+++ sortstrinascending''.py
@@ -0,0 +1,7 @@
+N=int(input())
+s=list(map(str,input().split()))
+p=sorted(s,key=len)
+for i in range(len(p)-1):
+  if len(p[i])==len(p[i+1]) and p[i]>p[i+1]:
+    p[i],p[i+1]=p[i+1],p[i]
+print(*p)
